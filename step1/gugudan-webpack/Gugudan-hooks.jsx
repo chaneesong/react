65240b1f12e31b58aa1a8fe8c5b4d771d7374613
kacohead96 <sch102403@gmail.com>
2022-10-19 22:06:08 +0900

    주석 제거

--- step1/gugudan-webpack/Gugudan-hooks.jsx
+++ step1/gugudan-webpack/Gugudan-hooks.jsx
@@ -1,55 +1,6 @@
 const React = require('react');
 const { Component, useRef } = React;
 
-// class Gugudan extends Component {
-//   state = {
-//     first: Math.floor(Math.random() * 9) + 1,
-//     second: Math.floor(Math.random() * 9) + 1,
-//     value: '',
-//     result: '',
-//   };
-
-//   onSubmit = (e) => {
-//     e.preventDefault();
-//     if (this.state.first * this.state.second === parseInt(this.state.value)) {
-//       this.setState({
-//         result: '정답' + ' ' + this.state.value,
-//         value: '',
-//         first: Math.floor(Math.random() * 9) + 1,
-//         second: Math.floor(Math.random() * 9) + 1,
-//       });
-//     } else {
-//       this.setState({
-//         result: '오답' + ' ' + this.state.value,
-//         value: '',
-//       });
-//     }
-//   };
-
-//   onChange = (e) => {
-//     this.setState({
-//       value: e.target.value,
-//     });
-//   };
-
-//   render() {
-//     return (
-//       <div>
-//         <div>
-//           {this.state.first} X {this.state.second}
-//         </div>
-//         <form onSubmit={this.onSubmit}>
-//           <input type='number' value={this.state.value} onChange={this.onChange} />
-//           <button>입력</button>
-//         </form>
-//         <div>
-//           {this.state.answer} {this.state.result}
-//         </div>
-//       </div>
-//     );
-//   }
-// }
-
 const Gugudan = () => {
   const [first, setFirst] = React.useState(Math.floor(Math.random() * 9) + 1);
   const [second, setSecond] = React.useState(Math.floor(Math.random() * 9) + 1);
