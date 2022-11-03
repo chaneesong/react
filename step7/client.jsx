4764fc519b81eeb4f8f916aba39b66c361a778ee
gigo96 <sch102403@gmail.com>
2022-11-03 22:35:44 +0900

    지뢰찾기 구현

---
+++ step7/client.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import MineSearch from './MineSearch';
+
+const root = ReactDOM.createRoot(document.querySelector('#root'));
+root.render(<MineSearch />);
